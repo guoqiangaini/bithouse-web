27fd4af5e009128138a4437bebc38ea18e6fe2e9
tengwenliang <214175824@qq.com>
2018-04-09 19:05:44 +0800

    header

--- src/loginAndRegister/Header.vue
+++ src/loginAndRegister/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row style="height:10%">
-    <el-header class="wai" style="height:100%">
+    <el-header class="wai" style="height:100%;overflow: hidden;">
       <el-col :span="3" class="user" ><img src="https://pro.modao.cc/uploads3/images/1751/17519267/raw_1520225943.png">
       </el-col>
       <el-col :span="13" class="font" style="font-size: 28px;font-weight: 600;">{{CorporateName}}</el-col>
@@ -28,7 +28,7 @@
         <!--width: 32px;height: 32px;padding-top: 29px" v-popover:popover3></el-badge>-->
         <img
         src="https://pro.modao.cc//uploads3/images/1783/17836132/raw_1521017810.png"
-        style="width: 32px;height: 32px;"  v-popover:popover3 @click="dotState" >
+        style="cursor: pointer"  v-popover:popover3 @click="dotState" >
         <div class="tip" v-show="dot" ></div>
       </el-col>
       <el-col :span="2" class="right_icon">
@@ -41,8 +41,7 @@
             <el-button  @click="outSystem">退出</el-button>
           </el-col>
         </el-popover>
-        <img src="https://pro.modao.cc//uploads3/images/1783/17836137/raw_1521017812.png
-" style="width: 32px;height: 32px;cursor: pointer" v-popover:popover4>
+        <img src="https://pro.modao.cc//uploads3/images/1783/17836137/raw_1521017812.png" style="cursor: pointer" v-popover:popover4>
 
 
       </el-col>
@@ -51,141 +50,136 @@
   </el-row>
 </template>
 <script>
-  import axios from 'axios'
-  import qs from 'qs'
-  import echarts from 'echarts'
-  import ElRow from "element-ui/packages/row/src/row";
-  import ElCol from "element-ui/packages/col/src/col";
-  export default {
-    components: {
-      ElCol,
-      ElRow
+import axios from "axios";
+import qs from "qs";
+import echarts from "echarts";
+import ElRow from "element-ui/packages/row/src/row";
+import ElCol from "element-ui/packages/col/src/col";
+export default {
+  components: {
+    ElCol,
+    ElRow
+  },
+  name: "hello",
+  data() {
+    return {
+      CorporateName: "", //公司名称
+      todayTime: "", //获取时间
+      dot: false,
+      information: []
+    };
+  },
+  methods: {
+    dotState() {
+      this.dot = false;
     },
-    name: 'hello',
-    data () {
-      return {
-        CorporateName: '',//公司名称
-        todayTime: '',//获取时间
-        dot:false,
-        information:[]
-      }
+    dotCondition() {
+      this.dot = true;
     },
-    methods:{
-      dotState(){
-        this.dot=false;
-      },
-      dotCondition(){
-
-        this.dot=true;
-      },
-      outSystem: function () {
-        sessionStorage.removeItem("userData");
-        this.$router.push('/')
-      }
-    },
-    mounted(){
-      if(this.information != ''){
-          this.dot=true
-      } else{
-        this.dot=false
-      }
-      var that = this
-      //获取时间
-      var timeParams = {
-        methodUrl: 'date/getDate',
-        jsonParam: qs.stringify()
-      }
-      this.$axios.postRequest(timeParams).then(function (res) {
+    outSystem: function() {
+      sessionStorage.removeItem("userData");
+      this.$router.push("/");
+    }
+  },
+  mounted() {
+    if (this.information != "") {
+      this.dot = true;
+    } else {
+      this.dot = false;
+    }
+    var that = this;
+    //获取时间
+    var timeParams = {
+      methodUrl: "date/getDate",
+      jsonParam: qs.stringify()
+    };
+    this.$axios.postRequest(timeParams).then(
+      function(res) {
         //成功之后处理逻辑
-        that.todayTime = res.data.time
-
-      }, function (res) {
+        that.todayTime = res.data.time;
+      },
+      function(res) {
         //失败之后处理逻辑
-        console.log("error:" + res)
-      })
-
-      //请求公司名称
-      var userData = qs.parse(sessionStorage.getItem("userData"));
-      that.CorporateName = userData.company_name;
-      //请求消息
-      var infomationData={
-        regserial:userData.company_serial,
-        news_role:'103',
-        user_serial:userData.employee_serial
-      };
-      var infomationParams={
-        methodUrl:'adminMenu/getInformInfo',
-        jsonParam:qs.stringify(infomationData),
+        console.log("error:" + res);
       }
-      this.$axios.postRequest(infomationParams).then(function(res) {
+    );
+
+    //请求公司名称
+    var userData = qs.parse(sessionStorage.getItem("userData"));
+    that.CorporateName = userData.company_name;
+    //请求消息
+    var infomationData = {
+      regserial: userData.company_serial,
+      news_role: "103",
+      user_serial: userData.employee_serial
+    };
+    var infomationParams = {
+      methodUrl: "adminMenu/getInformInfo",
+      jsonParam: qs.stringify(infomationData)
+    };
+    this.$axios.postRequest(infomationParams).then(
+      function(res) {
         //成功之后处理逻辑
-        that.information = res.data
-
-      }, function(res) {
+        that.information = res.data;
+      },
+      function(res) {
         //失败之后处理逻辑
-        console.log("error:" + res)
-      })
-    },
-
-
-
+        console.log("error:" + res);
+      }
+    );
   }
+};
 </script>
 <style scoped>
-  .wai {
-    background-color: #1b96a9;
+.wai {
+  background-color: #1b96a9;
+}
 
-  }
+.user {
+  background-color: #1b96a9;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  height: 100%;
+  text-align: center;
+}
 
-  .user {
-    background-color: #1b96a9;
-    position: relative;
-    top: 50%;
-    transform: translateY(-50%);
-    height: 100%;
-  }
+.user img {
+  height: 100%;
+}
 
-  .user img {
-    width: 64px;
-   height: 100%;
-
-  
-    vertical-align: middle;
-    margin-left: 45px;
-
-  }
-
-  .font {
-    color: #fff;;
-    font-size: 14px;
-    height: 100%;
-    line-height: 64px;
-    position: relative;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-  .right_icon {
-    position: relative;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-  .tip {
-    min-width: 14px;
-    height: 14px;
-    background: red;
-    box-sizing: border-box;
+.font {
+  color: #fff;
+  font-size: 14px;
+  position: relative;
+  top: 47%;
+  transform: translateY(-50%);
+}
+.right_icon {
+  height: 100%;
+  text-align: center;
+}
+.right_icon img {
+  height: 40%;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.tip {
+  min-width: 14px;
+  height: 14px;
+  background: red;
+  box-sizing: border-box;
   color: white;
-    font-size: 10px;
-    text-align: center;
-    line-height: 20px;
-    padding: 0 5px;
-    border-radius: 10px;
-    display: inline-block;
-    position: inherit;
-    z-index: 1111;
-    margin-bottom: 20px;
-    margin-left: -12px;
-
-  }
-
+  font-size: 10px;
+  text-align: center;
+  line-height: 20px;
+  padding: 0 5px;
+  border-radius: 10px;
+  display: inline-block;
+  position: inherit;
+  z-index: 1111;
+  margin-bottom: 20px;
+  margin-left: -12px;
+}
 </style>
